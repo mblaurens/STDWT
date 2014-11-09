6b06bf8c287e00338fdea0cc237959db7046575f
Richard Mei <richard.mei97@gmail.com>
2014-11-09 03:09:13 -0500

    started skill lists

--- Turtle/Turtle.java
+++ Turtle/Turtle.java
@@ -4,9 +4,11 @@ public class Turtle{
     private int hungerCap,thirstCap,hunger,thirst;
     private int baseStr,curStr,baseInt,curInt,baseDex,curDex;
     private String name;
+    private int level,talents,curXP,reqXP;
     
     public Turtle(String n){
 	name=n;
+	level = 0; talents = 0;
 	baseHP=10;curHP=10;
 	baseMP=10;curMP=10;
 	hungerCap=15;hunger=0;
@@ -14,6 +16,7 @@ public class Turtle{
 	baseStr=5;curStr=5;
 	baseInt=5;curInt=5;
 	baseDex=5;curDex=5;
+	curXP = 0;reqXP = 0;
     }
 
     public Turtle(){
@@ -28,6 +31,19 @@ public class Turtle{
 		+"\tWisdom:"+curInt+"/"+baseInt
 		+"\tSlipperyness:"+curDex+"/"+baseDex;
     }
-    
+	public void ding(){
+		if (curXP >= reqXP) {
+			level++;
+			talents++;
+			baseHP += 5; curHP = baseHP;   //just a template these numbers might not be balanced 
+			baseMP += 5; curMP = baseMP;
+			baseStr += 2; curStr =+ 2;
+			baseDex += 2; curDex =+ 2;
+			baseInt += 2; curInt =+ 2;
+			curXP = 0;
+			reqXP = 5;  // need leveling up system
+		}
+
+	}    
 }
    
